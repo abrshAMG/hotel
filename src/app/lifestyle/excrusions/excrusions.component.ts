594a67f40c6715dc96ba2e89424c5e4b90056bb9
abreham <abrsh.mulu10@gmail.com>
2020-10-21 19:21:33 +0300

    Initial Commit

---
+++ src/app/lifestyle/excrusions/excrusions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excrusions',
+  templateUrl: './excrusions.component.html',
+  styleUrls: ['./excrusions.component.css']
+})
+export class ExcrusionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
